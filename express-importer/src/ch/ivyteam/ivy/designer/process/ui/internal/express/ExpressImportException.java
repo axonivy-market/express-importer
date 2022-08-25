7ed9a296995d973618a7ad8c83b6b209aabd6e7a
Bruno Bütler <bruno.buetler@axonivy.com>
2022-08-25 12:46:43 +0200

    WIP: import-wizard for express workflows

---
+++ express-importer/src/ch/ivyteam/ivy/designer/process/ui/internal/express/ExpressImportException.java
@@ -0,0 +1,18 @@
+package ch.ivyteam.ivy.designer.process.ui.internal.express;
+public class ExpressImportException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+  public ExpressImportException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ExpressImportException(String message) {
+    super(message);
+  }
+
+  public ExpressImportException(Throwable cause) {
+    super(cause);
+  }
+
+}
---
+++ express-importer/src/ch/ivyteam/ivy/designer/process/ui/internal/express/ExpressImportWizard.java
@@ -0,0 +1,43 @@
+package ch.ivyteam.ivy.designer.process.ui.internal.express;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IExportWizard;
+import org.eclipse.ui.IWorkbench;
+
+import ch.ivyteam.ivy.designer.ide.DesignerIDEPlugin;
+
+public class ExpressImportWizard extends Wizard implements IExportWizard {
+
+  private ExpressImportProcessor processor;
+
+  public ExpressImportWizard() {
+    setWindowTitle("Import");
+    var workbenchSettings = DesignerIDEPlugin.getDefault().getDialogSettings();
+    var wizardSettings = workbenchSettings.getSection(ExpressImportWizard.class.getName());
+    if (wizardSettings == null) {
+      wizardSettings = workbenchSettings.addNewSection(ExpressImportWizard.class.getName());
+    }
+    setDialogSettings(wizardSettings);
+  }
+
+  @Override
+  public void init(IWorkbench workbench, IStructuredSelection selection) {
+    processor = new ExpressImportProcessor(selection);
+  }
+
+  @Override
+  public void addPages() {
+    addPage(new ExpressImportWizardPage(processor));
+  }
+
+  @Override
+  public boolean performFinish() {
+    return ((ExpressImportWizardPage) getPage(ExpressImportWizardPage.PAGE_ID)).finish();
+  }
+
+  @Override
+  public boolean needsProgressMonitor() {
+    return true;
+  }
+}
